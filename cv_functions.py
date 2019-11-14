e7508a5c2896bd4f28c4672c7d5bf99838e95525
Brendon Forsgren <bforsgren29@gmail.com>
2019-11-14 14:17:21 -0700

    Got the HSV rough boundarys. Red was a littl weird. Will want to test with the actual set up and the camera on baxter to fine tune

--- cv_functions.py
+++ cv_functions.py
@@ -9,14 +9,15 @@ def extractColors(hsv):
     blue_low = np.array([100, 50, 50]) #Color bounds for the blue picture
     blue_high = np.array([130, 255, 255])
 
-    red_low = np.array([110, 50, 50])
-    red_high = np.array([130, 255, 255])
+    red_low = np.array([10, 10, 10])
+    red_high = np.array([150, 255, 255])
 
-    green_low = np.array([110, 50, 50])
-    green_high = np.array([130, 255, 255])
+    green_low = np.array([60, 50, 50])
+    green_high = np.array([90, 255, 255])
 
     img_b = cv2.inRange(hsv, blue_low, blue_high)
     img_r = cv2.inRange(hsv, red_low, red_high)
+    img_r = cv2.bitwise_not(img_r)
     img_g = cv2.inRange(hsv, green_low, green_high)
 
     return img_b, img_g, img_r    
@@ -24,8 +25,8 @@ def extractColors(hsv):
 
 if __name__ == "__main__":
     #This is for my tests. I'll just call the above functions form main.py when doing the implementation.
-    # img1 = cv2.imread("racquetball_red_green.jpg")
-    img1 = cv2.imread("racquetball_blue_green.jpg")
+    img1 = cv2.imread("racquetball_red_green.jpg")
+    # img1 = cv2.imread("racquetball_blue_green.jpg")
 
     img1_hsv = convertToHSV(img1)
 
